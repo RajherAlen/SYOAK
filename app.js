86bd9b76bef2c8d1255812b71609653d82c0ab9a
Alen <alen.rajher@gmail.com>
2020-09-01 18:46:35 +0200

    dosao do footera

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const checkBox = document.querySelector(".checkBox");
 checkBox.addEventListener("click", (e) => {
   e.target.src === "http://127.0.0.1:5500/img/checked.svg"
     ? (e.target.src = "./img/checkOutline.svg")
-    : (e.target.src = "./img/checked.svg");
+    : (e.target.src = "http://127.0.0.1:5500/img/checked.svg");
 });
