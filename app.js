f90b96066857f70b2480e433fec6194aa9882099
Alen <alen.rajher@gmail.com>
2020-09-01 20:21:44 +0200

    .

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const checkBox = document.querySelector(".checkBox");
 
 checkBox.addEventListener("click", (e) => {
+  console.log(e.target.src);
   e.target.src === "http://127.0.0.1:5500/img/checked.svg"
     ? (e.target.src = "./img/checkOutline.svg")
     : (e.target.src = "http://127.0.0.1:5500/img/checked.svg");
