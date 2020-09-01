f435497e8207f3544448d18aa89a771922461845
RajherAlen <alen.rajher@gmail.com>
2020-09-01 20:24:00 +0200

    .

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const checkBox = document.querySelector(".checkBox");
 checkBox.addEventListener("click", (e) => {
   console.log(e.target.src);
   e.target.src === "https://gracious-boyd-197cee.netlify.app/img/checked.svg"
-    ? (e.target.src = "./img/checkOutline.svg")
+    ? (e.target.src =
+        "https://gracious-boyd-197cee.netlify.app/img/checkOutline.svg")
     : (e.target.src =
         "https://gracious-boyd-197cee.netlify.app/img/checked.svg");
 });
